42a2c6bc5762bf1f17ccbe010f8c794ee10e8384
Morten Just <morten.just@genstart.dk>
2022-04-04 08:57:22 +0200

    Initial Commit

---
+++ WebViewTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WebViewTest
+//
+//  Created by Morten Just on 4/4/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
